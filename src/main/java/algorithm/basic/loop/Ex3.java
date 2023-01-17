769ab7a8b026bc39beb2ee423b67db270b422879
duck <newve1@naver.com>
2023-01-17 09:40:34 +0900

    commit!

--- src/main/java/algorithm/basic/loop/Ex3.java
+++ src/main/java/algorithm/basic/loop/Ex3.java
@@ -3,28 +3,43 @@ package algorithm.basic.loop;
 import java.util.Scanner;
 
 public class Ex3 {
-
-    // *를 n개 출력하되 w개 마다 줄을 바꿔서 출력
-    // n은 총 *수, w는 한줄에 들어가는 *수
-    // n값보다 w가 크면 w 다시쓰라고 나오기 ("w값:")
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("n값 : ");
-        int n= input.nextInt();
+        System.out.print("n값: ");
+        int n = input.nextInt();
 
-        System.out.print("w값 : ");
-        int w =input.nextInt();
+        System.out.print("w값: ");
+        int w = input.nextInt();
 
-        int division = n/w;
 
-        for (int i = 1; i <= division; i++){
-            System.out.println(i);
+        if (n >= w) {
+
+            printStar(n, w);
+
+        } else {
+            System.out.println("w가 더 큽니다.");
+            System.out.print("w값: ");
+            w = input.nextInt();
+
+            printStar(n, w);
+
         }
 
+    }
 
+    private static void printStar(int n, int w) {
+        int division = n / w;
+        int remainder = n % w;
 
-
+        for (int i = 1; i <= division; i++) {
+            for (int j = 1; j <= w; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int k = 1; k <= remainder; k++) {
+            System.out.print("*");
+        }
     }
 }
