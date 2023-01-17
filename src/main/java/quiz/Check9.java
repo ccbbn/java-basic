75ecf591e0f4525461372a2df8773f769f70676f
duck <newve1@naver.com>
2023-01-18 08:59:38 +0900

    commit!

--- src/main/java/quiz/Check9.java
+++ src/main/java/quiz/Check9.java
@@ -1,6 +1,7 @@
 package quiz;
 
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Check9 {
@@ -11,35 +12,51 @@ public class Check9 {
         Scanner scanner =new Scanner(System.in);
 
         while(run) {
-            System.out.println("__________________________________");
+            System.out.println("---------------------------------------------------");
             System.out.println(" 1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료 ");
-            System.out.println("__________________________________");
+            System.out.println("---------------------------------------------------");
             System.out.print("선택> ");
 
             int selectNo = scanner.nextInt();
 
-            if(selectNo == 1) {
+            if (selectNo == 1) {
                 System.out.print("학생수> ");
-                int input = scanner.nextInt();
-                studentNum = input;
+                studentNum = scanner.nextInt();
+
             } else if (selectNo == 2) {
+                scores = new int[studentNum]; // scores[]의 크기 정함
+
                 for (int i = 0; i < studentNum; i++) {
-                    System.out.print("scores["+i+"]" +">");
-                    int input = scanner.nextInt();
-                    scores[i] = input;
+                    System.out.print("scores["+i+"]" + "> ");
+                    scores[i] = scanner.nextInt();
                 }
+
             } else if (selectNo == 3) {
-
-
+                for (int i = 0; i < studentNum; i++) {
+                    System.out.println("scores["+i+"]" + ": " + scores[i]);
+                }
 
             } else if (selectNo == 4) {
+                int max = 0;
+                int sum = 0;
+                // max 구하기
+                for (int i = 0; i < studentNum; i++) {
+                    if (scores[i] > max) {
+                        max = scores[i];
+                    }
+                }
+                // sum 구하기
+                for (int i = 0; i < studentNum; i++) {
+                    sum += scores[i];
+                }
+
+                System.out.println("최고 점수: " + max);
+                System.out.println("평균 점수: " + (double)sum/studentNum);
 
             } else if (selectNo == 5) {
                 run = false;
             }
         }
-
-
         System.out.println("프로그램 종료");
     }
 }
