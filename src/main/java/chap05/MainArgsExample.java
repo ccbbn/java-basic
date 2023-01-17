199ad6f8d6392b79bc441c22b7fa31a7bdb24c14
duck <newve1@naver.com>
2023-01-17 15:24:27 +0900

    commit!

--- src/main/java/chap05/MainArgsExample.java
+++ src/main/java/chap05/MainArgsExample.java
@@ -3,6 +3,6 @@ package chap05;
 public class MainArgsExample {
     public static void main(String[] args) {
         // 문자열 배열
-        System.out.println(args[0]);
+        System.out.println();
     }
 }
