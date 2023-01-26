d2ee0bdb1719e7b6a075a1c879ddaafa3f7af6c7
ccbbn <newve1@naver.com>
2023-01-27 02:59:10 +0900

    !~

--- src/main/java/chap06/check/Q20Ex.java
+++ src/main/java/chap06/check/Q20Ex.java
@@ -1,5 +1,7 @@
 package chap06.check;
 
+import jdk.swing.interop.DropTargetContextWrapper;
+
 import java.sql.Struct;
 import java.util.Scanner;
 
@@ -23,39 +25,86 @@ public class Q20Ex {
                 accountList();
             } else if (selectNo == 3) {
                 deposit();
-            } else if (selectNo == 4) {
-                withdraw();
-            } else if (selectNo == 5) {
+//            } else if (selectNo == 4) {
+//                withdraw();
+//            } else if (selectNo == 5) {
                 run = false;
             }
         }
-            System.out.println("프로그램 종료");
-        }
-        //계좌생성하기
-        private static void createAccount() {
-            System.out.println("----------------");
-            System.out.println("계좌생성");
-            System.out.println("----------------");
-            System.out.print("계좌번호: ");
-            String accNum = scanner.nextLine();
-            System.out.print("계좌주: ");
-            String name = scanner.nextLine();
-            System.out.print("초기입금액: ");
-            int save = scanner.nextInt();
-            System.out.println("결과: 계좌가 생성되었습니다.");
-            Q20 row = new Q20(accNum, name, save);
-            int cunt = 0;
-            accountArray[cunt++] = row;
+        System.out.println("프로그램 종료");
 
-        }
+    }
 
-        //계좌목록보기
-        private static void accountList() {
-            System.out.println();
+    //계좌생성하기
+    private static void createAccount() {
+        System.out.println("----------------");
+        System.out.println("계좌생성");
+        System.out.println("----------------");
+
+        System.out.print("계좌번호: ");
+        String ano = scanner.next();
+
+        System.out.print("계좌주: ");
+        String owner = scanner.next();
+
+        System.out.print("초기입금액: ");
+        int balance = scanner.nextInt();
+
+        System.out.println("결과: 계좌가 생성되었습니다.");
+        Q20 row = new Q20(ano, owner, balance);
+
+        for (int i = 0; i < accountArray.length; i++) {
+            if (accountArray[i] == null) {
+                accountArray[i] = row;
+                break;
+            }
+        }
+    }
+
+    //계좌목록보기
+    private static void accountList() {
+        int i = 0;
+        while (accountArray[i] != null) {
+            System.out.println(accountArray[i].getAno() +
+                    "     " + accountArray[i].getOwner() +
+                    "      " + accountArray[i].getBalance());
+            i++;
+        }
+    }
+
+    // 예금하기
+    private static void deposit() {
+        System.out.print("계좌번호: ");
+        String ano = scanner.next();
+        System.out.print("예금액: ");
+        int balance = scanner.nextInt();
+
+        findAccount(ano);
+
 
 
     }
+
+    // 출금하기
+    private static void withdraw() {
+
+    }
+
+    //Account(Q20) 배열에서 ano와 동일한 Account 객체 찾기
+    private static Q20 findAccount(String ano) {
+        Q20 find = null;
+        for (int i = 0; i < accountArray.length; i++) {
+            if (ano == accountArray[i].getAno()) {
+                find = accountArray[i];
+
+            }
+        }
+        return find;
+    }
+
 }
+
+
 
 
 
