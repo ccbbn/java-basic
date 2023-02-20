0bf70bdc552d7242eb74b600eca1d93ff40a7440
ccbbn <newve1@naver.com>
2023-02-20 22:38:55 +0900

    commit!

--- src/main/java/chap18/q.java
+++ src/main/java/chap18/q.java
@@ -1,22 +1,70 @@
 package chap18;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class q {
 
+    static Scanner scanner = new Scanner(System.in);
+    static int a = (int) (Math.random() * 100 + 1);
+    static int count = 10;
+
     public static void main(String[] args) {
-
-        int answer = 0;
-
-        String num1 = Integer.toString(12345);
-
-        String[] arr = num1.split("");
-
-        for (int i = 0; i < num1.length(); i++) {
-            answer += Integer.parseInt(arr[i]);
-
+        game();
+    }
+    private static void game() {
+        while (count > 0) {
+            System.out.print("숫자를 맞춰봐 : ");
+            int insert = scanner.nextInt();
+            if (insert > a) {
+                System.out.println("down");
+                count--;
+                System.out.println("시도 횟수 : " + (10 - count));
+                System.out.println("==========================");
+            } else if (insert < a) {
+                System.out.println("up");
+                count--;
+                System.out.println("시도 횟수 : " + (10 - count));
+                System.out.println("==========================");
+            } else {
+                System.out.println("☆★☆★성공☆★☆★ ");
+                System.out.println("시도 횟수 : " + (10 - count));
+                System.out.println("==========================");
+                count = -1;
+            }
         }
-        System.out.println(answer);
+        if (count == 0) {
+            System.out.println("실패!");
+            reGame();
+        } else
+            reGame();
+    }
+
+    private static void reGame() {
+        boolean run = true;
+        while (run){
+            System.out.println("리겜 ㄱㄱ? y or n");
+            String reGame = scanner.next();
+            switch (reGame) {
+                case "y":
+                    System.out.println("리겜 고고");
+                    a = (int) (Math.random() * 100 + 1);
+                    count = 10;
+                    game();
+                    run = false;
+                    break;
+
+                case "n":
+                    System.out.println("그만하자");
+                    count = -1;
+                    run = false;
+                    break;
+
+                default:
+                    System.out.println("잘못 입력함");
+
+            }
+        }
     }
 
 }
