6da090e27ae28a37c4c32d9a4cb831091299dd0d
duck <newve1@naver.com>
2023-03-02 17:49:03 +0900

    commit!

--- src/main/java/aa.java
+++ src/main/java/aa.java
@@ -5,14 +5,33 @@ public class aa {
 
     public static void main(String[] args) {
 
-
-            solution(10,20);
-
+        int[][] x = {{1, 4}, {9, 2}, {3, 8}, {11, 6}};
+        solution(x);
     }
 
-    private static int solution(int num1, int num2) {
-        int answer = Stream.of(num1, num2).
-        return answer;
-    }
-}
+        private static int solution(int[][] dots) {
+            int answer = 0;
+            if (((dots[0][1] - dots[1][1]) / (dots[0][0] - dots[1][0]) == (dots[2][1] - dots[3][1]) / (dots[2][0] - dots[3][0])) || ((dots[0][1] - dots[1][1]) / (dots[0][0] - dots[1][0]) == -1*(dots[2][1] - dots[3][1]) / (dots[2][0] - dots[3][0])) ||
+                    ((dots[0][1] - dots[2][1]) / (dots[0][0] - dots[2][0]) == (dots[1][1] - dots[3][1]) / (dots[1][0] - dots[3][0])) || ((dots[0][1] - dots[2][1]) / (dots[0][0] - dots[2][0]) == -1*(dots[1][1] - dots[3][1]) / (dots[1][0] - dots[3][0])) ||
+                    ((dots[0][1] - dots[3][1]) / (dots[0][0] - dots[3][0]) == (dots[1][1] - dots[2][1]) / (dots[1][0] - dots[2][0])) || ((dots[0][1] - dots[3][1]) / (dots[0][0] - dots[3][0]) == -1*(dots[1][1] - dots[2][1]) / (dots[1][0] - dots[2][0]))
 
+            ) {
+                answer = 1;
+            }
+
+            return answer;
+
+
+//        public static String solution(String cipher, int code) {
+//            String answer = "";
+//            String[] a = cipher.split("");
+//            for( int i = 0; i < (cipher.length()/code); i++){
+//                answer += a[code*(i+1)-1];
+//
+//
+//        }
+//
+//            return answer;
+//
+//    }
+        }}
