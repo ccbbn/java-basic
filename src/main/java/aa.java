7efef44ed56e48630ef5cd69bedf47d19598df12
ccbbn <newve1@naver.com>
2023-03-06 01:29:41 +0900

    commit!

--- src/main/java/aa.java
+++ src/main/java/aa.java
@@ -13,48 +13,44 @@ public class aa {
         int[][] x = {{1, 4}, {9, 2}, {3, 8}, {11, 6}};
         String[] spell = {"p", "o", "s"};
         String[] dic = {"sod", "eocd", "qixm", "adio", "soo"};
-        solution("3x + 7 + x");
+        solution(new String[]{"3 - 4 = -3", "5 + 6 = 11"}); // (3,-,4,+,5)
 
 
     }
 
-    public String solution(String polynomial) {
-        String answer = "";
-        String[] a = polynomial.split(" \\+ ");
-        ArrayList<String> x = new ArrayList<>();
-        ArrayList<Integer> c = new ArrayList<>();
-        ArrayList<Integer> nx = new ArrayList<>();
-        ArrayList<String> sum = new ArrayList<>();
 
-        for (int i = 0; i < a.length; i++) {
-            if (a[i].contains("x")) {
-                x.add(a[i]);
-            } else
-                c.add(Integer.parseInt(a[i]));
-        }
-        for (int i = 0; i < x.size(); i++) {
-// 5x 1x 2x x
-            5x 1 2x
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        int x = 0;
+        int y = 0;
+        int z = 0;
 
-            if (Objects.equals(x.get(i), "x")  ) {
-                x.set(i, "1");
-            } else if (Objects.equals(x.get(i), "1x")) {
-                x.set(i, "1");
+
+        for (int i = 0; i < quiz.length; i++) {
+
+            String[] arrayQuiz = quiz[i].split(" ");
+
+            //x>0
+
+            x = Integer.parseInt(arrayQuiz[0]);
+
+            if (arrayQuiz[1].equals("-")) {
+                y = -1 * Integer.parseInt(arrayQuiz[2]);
+            } else {
+                y = Integer.parseInt(arrayQuiz[2]);
             }
-            x.set(i, x.get(i).replaceAll("[^\\d]", ""));
-            nx.add(Integer.parseInt(x.get(i)));
-        }
-        String xvalue = String.valueOf(nx.stream().mapToInt(Integer::intValue).sum());
-        String cvalue = String.valueOf(c.stream().mapToInt(Integer::intValue).sum());
 
-        if(xvalue.equals("0") && !cvalue.equals("0")) answer = cvalue;
-        if(cvalue.equals("0") && !xvalue.equals("0")) answer = xvalue+"x";
-        if(!cvalue.equals("0") && !xvalue.equals("0")) answer = xvalue+"x"+ " + " +cvalue;
-        if(xvalue.equals("0") && cvalue.equals("0")) answer = "0";
+            z = Integer.parseInt(arrayQuiz[4]);
 
-        return answer;
-        }
+
+            if (x + y == z) {
+                answer[i] = "O";
+            } else {
+                answer[i] = "X";
+            }
+
+        } return answer;
+
     }
-
-
+}
 
